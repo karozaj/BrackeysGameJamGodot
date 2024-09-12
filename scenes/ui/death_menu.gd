f34b622e05d962392e2a193d651cab6b542c1da0
karozaj <135151900+karozaj@users.noreply.github.com>
2024-09-12 12:54:01 +0200

    add enemy model

--- scenes/ui/death_menu.gd
+++ scenes/ui/death_menu.gd
@@ -7,6 +7,14 @@ func _ready() -> void:
 	var tween = get_tree().create_tween()
 	tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
 	tween.tween_property(self, "modulate", Color(1, 1, 1), 1)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+
+func _on_retry_button_pressed() -> void:
+	get_tree().paused=false
+	get_tree().change_scene_to_file("res://scenes/test_level.tscn")
+	queue_free()
+	
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
